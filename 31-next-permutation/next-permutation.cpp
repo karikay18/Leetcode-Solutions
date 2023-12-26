7fe556a679f6b3f0c727bbe9976ae817f4d89e7d
Kartikay Sharma <93234057+karikay18@users.noreply.github.com>
2023-12-26 11:32:07 +0530

    Time: 8 ms (13.56%) | Memory: 12.5 MB (55.07%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,7 +1,30 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
-        // return nums;
+    void nextPermutation(std::vector<int>& nums) {
+        int n = nums.size();
+        int ind = -1;
+
+        // Find the first decreasing element from the right
+        for (int i = n - 1; i > 0; i--) {
+            if (nums[i] > nums[i - 1]) {
+                ind = i;
+                break;
+            }
+        }
+
+        if (ind == -1) {
+            reverse(nums.begin(), nums.end()); // Reverse entire sequence
+            return;
+        }
+
+        // Find the smallest element larger than nums[ind-1] to swap with
+        for (int i = n - 1; i >= ind; i--) {
+            if (nums[i] > nums[ind - 1]) {
+                swap(nums[i], nums[ind - 1]);
+                break;
+            }
+        }
+
+        reverse(nums.begin() + ind, nums.end()); // Reverse the sequence from ind onwards
     }
 };
