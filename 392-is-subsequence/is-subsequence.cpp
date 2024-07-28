94a287bc02fae07b8585d0f5ebfbaf2b5cec6220
Kartikay Sharma <93234057+karikay18@users.noreply.github.com>
2024-07-28 15:37:27 +0530

    Time: 0 ms (100.00%) | Memory: 7.6 MB (49.09%) - LeetSync

---
+++ 392-is-subsequence/is-subsequence.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        // uordered_map<int>mpp;
+        int j=0;
+        for(int i=0;i<t.size();i++)
+        {
+            if(s[j]==t[i])
+            {
+                j++;
+            }
+        }
+            if(j==s.size())
+            return true;
+        return false;
+    }
+};
