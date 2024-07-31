486332436203653cf7d3c98b4668e2a3a006802e
Kartikay Sharma <93234057+karikay18@users.noreply.github.com>
2024-07-31 16:09:35 +0530

    Time: 98 ms (97.57%) | Memory: 81 MB (38.52%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> num_indices;
+        
+        for (int i = 0; i < nums.size(); i++) {
+            if (num_indices.find(nums[i]) != num_indices.end() && i - num_indices[nums[i]] <= k) {
+                return true;
+            }
+            num_indices[nums[i]] = i;
+        }
+        
+        return false;
+    }
+};
