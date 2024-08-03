e6eb15808d3240ea2ffe56446adbf2912ed3e4ad
Kartikay Sharma <93234057+karikay18@users.noreply.github.com>
2024-08-03 18:33:42 +0530

    Time: 15 ms (96.93%) | Memory: 22.9 MB (41.47%) - LeetSync

--- 56-merge-intervals/merge-intervals.cpp
+++ 56-merge-intervals/merge-intervals.cpp
@@ -1,27 +1,30 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-        vector<vector<int>>ans;
-        if(intervals.size()==0)
-        {
-            return ans;
-        }
-        sort(intervals.begin(),intervals.end());
-        vector<int>temp=intervals[0];
-        for(auto it:intervals)
-        {
-            if(it[0]<=temp[1])
-            {
-                temp[1]=max(it[1],temp[1]);
-            }
-            else
-            {
-                ans.push_back(temp);
-                temp=it;
+        if (intervals.empty()) return {};
 
+        // Sort intervals based on the start time
+        sort(intervals.begin(), intervals.end());
+
+        vector<vector<int>> res;
+        int s = intervals[0][0];
+        int e = intervals[0][1];
+
+        for (int i = 1; i < intervals.size(); ++i) {
+            if (intervals[i][0] <= e) {
+                // There is an overlap
+                e = max(e, intervals[i][1]);
+            } else {
+                // No overlap, push the current interval to result
+                res.push_back({s, e});
+                s = intervals[i][0];
+                e = intervals[i][1];
             }
         }
-        ans.push_back(temp);
-        return ans;
+
+        // Push the last interval
+        res.push_back({s, e});
+
+        return res;
     }
 };
