c487f71fe77bd88e5c66d8c80957bf779a8a7250
Kartikay Sharma <93234057+karikay18@users.noreply.github.com>
2024-08-10 18:25:34 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (26.23%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
-
-    int cal(int n, int r)
-    {
-        long long p=1, k=1;
-        if (n-r<r)
-            r=n-r;
-        if(r!=0) {
-            while(r) {
-                p*=n;
-                k*=r;
-                long long m=__gcd(p,k);
-                p/=m;
-                k/=m;
-                n--;
-                r--;
-            }
-        }
-        else
-            p=1;
-        return p;
+    int countPaths(int i, int j, vector<vector<int>>& dp) {
+        if (i < 0 || j < 0)
+            return 0;
+        if (i == 0 && j == 0)
+            return 1;
+        if(dp[i][j] != -1) 
+            return dp[i][j];
+        
+        int rp = countPaths(i, j - 1, dp);
+        int lp = countPaths(i - 1, j, dp);
+        return dp[i][j] = rp + lp;
     }
-    int uniquePaths(int m, int n) {
-        return cal(m+n-2,n-1);
+    
+    int uniquePaths(int m, int n) { 
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return countPaths(m-1, n-1, dp);
     }
 };
