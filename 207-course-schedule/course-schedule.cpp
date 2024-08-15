8d5067fc67ecd0aa10ca38cb667f470a6f9a3333
Kartikay Sharma <93234057+karikay18@users.noreply.github.com>
2024-08-15 14:49:56 +0530

    Time: 16 ms (57.00%) | Memory: 17.2 MB (73.06%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,39 +1,35 @@
 class Solution {
 public:
     bool canFinish(int n, vector<vector<int>>& prerequisites) {
-        vector<int> adj[n];
+        vector<vector<int>> adj(n);
         vector<int> indegree(n, 0);
-
-        // Populate the adjacency list and calculate indegrees
-        for (auto it : prerequisites) {
-            adj[it[1]].push_back(it[0]); // Assuming prerequisites are given as pairs [a, b] where b is a prerequisite for a
-            indegree[it[0]]++;
+        
+        for (const auto& prereq : prerequisites) {
+            adj[prereq[1]].push_back(prereq[0]);
+            indegree[prereq[0]]++;
         }
-
+        
         queue<int> q;
-        // Push nodes with 0 indegree into the queue
-        for (int i = 0; i < n; ++i) {
+        for (int i = 0; i < n; i++) {
             if (indegree[i] == 0) {
                 q.push(i);
             }
         }
-
-        vector<int> topo;
+        
+        vector<int> res;
         while (!q.empty()) {
-            int node = q.front();
+            int curr = q.front();
             q.pop();
-            topo.push_back(node);
-
-            // Decrement indegree of adjacent nodes and push if their indegree becomes 0
-            for (auto it : adj[node]) {
-                indegree[it]--;
-                if (indegree[it] == 0) {
-                    q.push(it);
+            res.push_back(curr);
+            
+            for (int next : adj[curr]) {
+                indegree[next]--;
+                if (indegree[next] == 0) {
+                    q.push(next);
                 }
             }
         }
-
-        // If the size of topo is equal to n, it means all courses can be completed
-        return topo.size() == n;
+        
+        return res.size() == n;
     }
 };
